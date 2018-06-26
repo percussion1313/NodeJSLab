06da0f42cb0a0754742b895e4fb86d8562b253ad
percussion1313 <peterjacobgrant@gmail.com>
2018-06-26 10:02:41 -0500

    Completed phase 2. Added reddit json with popular articles.

--- reddit.js
+++ reddit.js
@@ -8,21 +8,28 @@ let redditPath = path.join(__dirname, './popular-articles.json');
 const options = {
     method: 'GET',
     uri: 'https://reddit.com/r/popular.json'
-  }
-
-rp(options)
-    .then((results) => {
-        JSON.parse(results).data.children.forEach(item => {
-            
-            let redditArray = [];
-            let redditObj = {Title: item.data.title, URL: item.data.url, Author: item.data.author}           
-            redditArray.push(redditObj)
-
-            fs.writeFileSync(redditPath,  JSON.stringify(redditArray));
-        });
-    }).catch((err) => {
-        console.log(err)
-    })
+}
 
 
+let redditArray = [];
+
+rp(options, (err, res, body) => {
     
+    let redditData = [];
+    if (err) console.log(err);
+
+    redditData = JSON.parse(body).data.children.forEach(item => {
+        redditArray.push({
+            "title": item.data.title,
+            "author": item.data.author,
+            "url": item.data.url
+        });
+})
+
+    fs.writeFileSync(redditPath, JSON.stringify(redditArray), err => {
+        if (err) console.log(err)
+    });
+});
+
+
+
